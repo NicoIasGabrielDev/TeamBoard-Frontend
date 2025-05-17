6e64ecb86af6df9f655dc165f0d08ab925fd62bd
Nicolas <nickcr7cardoso@gmail.com>
2025-05-17 21:55:26 +0100

    Error fix

--- src/components/calendar/CalendarBoard.tsx
+++ src/components/calendar/CalendarBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import { format, isSameDay } from 'date-fns';
